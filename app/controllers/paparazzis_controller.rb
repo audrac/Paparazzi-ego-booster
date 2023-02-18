02c05e3dae45b8cd55d5c6d1ccc66570f22e5ecd
audrone cirkelyte <audracirk@gmail.com>
2023-02-18 17:53:00 +0100

    Seeding paparazzis

--- app/controllers/paparazzis_controller.rb
+++ app/controllers/paparazzis_controller.rb
@@ -1,5 +1,15 @@
 class PaparazzisController < ApplicationController
+
   def index
-    paparazzi = Paparazzi.all
+    @paparazzi = Paparazzi.all
+  end
+
+  def show
+  end
+
+  private
+
+  def paparazzi_params
+    params.require(:paparazzi).permit(:name, :price, :rating, :location, :style, :photo)
   end
 end
