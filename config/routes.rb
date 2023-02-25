2225d07e0be1e384ed8de1c190baecb1d07600cd
Darko Iliev <darkoilievsum@gmail.com>
2023-02-25 16:09:16 +0100

    delete booking

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/about", to: "pages#about"
   get "/dashboard", to: "pages#dashboard"
+  delete "/bookings/:id", to: "bookings#destroy", as: :delete_booking
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
